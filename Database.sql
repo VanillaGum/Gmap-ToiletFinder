0e12124cd971a3b79c444ff1eeede1c97cbc2f9c
VanillaGum <32533568+VanillaGum@users.noreply.github.com>
2018-01-08 23:24:48 +0800

    Added Database SQL File

--- Database.sql
+++ Database.sql
@@ -1,3 +1,7 @@
+/*
+DROP DATABASE toilet_finder;
+*/
+
 CREATE DATABASE toilet_finder;
 
 USE toilet_finder;
@@ -7,4 +11,23 @@ CREATE TABLE toilet (
 	name VARCHAR(200),
     latitude double,
     longitude double
+);
+
+CREATE TABLE toilet_info (
+	id int auto_increment UNIQUE PRIMARY KEY NOT NULL,
+    toilet_id int NOT NULL,
+    rating int DEFAULT 0,
+    amt_of_rating int DEFAULT 0,
+    genderM boolean NOT NULL
+);
+
+CREATE TABLE toilet_request (
+	id int auto_increment UNIQUE PRIMARY KEY NOT NULL,
+    latitude double NOT NULL,
+    longitude double NOT NULL,
+    approval int DEFAULT 0,
+    rating int DEFAULT 0,
+    amt_of_rating int DEFAULT 0,
+    genderM boolean,
+    removal_flags int DEFAULT 0
 );
